91b9aae23d006c5000320abc29418014eb38bff0
TacoTheDank <SkytkRSfan3895@gmail.com>
2021-01-14 14:52:56 -0500

    Further SettingsHistoryFragment cleanup

--- app/src/main/java/me/ccrama/redditslide/Fragments/SettingsHistoryFragment.java
+++ app/src/main/java/me/ccrama/redditslide/Fragments/SettingsHistoryFragment.java
@@ -3,9 +3,9 @@ package me.ccrama.redditslide.Fragments;
 import android.app.Activity;
 import android.widget.RelativeLayout;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.widget.SwitchCompat;
 
-import com.afollestad.materialdialogs.AlertDialogWrapper;
 import com.lusfold.androidkeyvaluestore.KVStore;
 
 import me.ccrama.redditslide.R;
@@ -33,7 +33,7 @@ public class SettingsHistoryFragment {
         storeHistorySwitch.setChecked(SettingValues.storeHistory);
         storeHistorySwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             SettingValues.storeHistory = isChecked;
-            SettingValues.prefs.edit().putBoolean(SettingValues.PREF_STORE_HISTORY, isChecked).apply();
+            editSharedPreference(SettingValues.PREF_STORE_HISTORY, isChecked);
 
             if (isChecked) {
                 scrollSeenSwitch.setEnabled(true);
@@ -42,40 +42,46 @@ public class SettingsHistoryFragment {
                 storeNsfwHistorySwitch.setChecked(false);
                 storeNsfwHistorySwitch.setEnabled(false);
                 SettingValues.storeNSFWHistory = false;
-                SettingValues.prefs.edit().putBoolean(SettingValues.PREF_STORE_NSFW_HISTORY, false).apply();
+                editSharedPreference(SettingValues.PREF_STORE_NSFW_HISTORY, false);
 
                 scrollSeenSwitch.setChecked(false);
                 scrollSeenSwitch.setEnabled(false);
                 SettingValues.scrollSeen = false;
-                SettingValues.prefs.edit().putBoolean(SettingValues.PREF_SCROLL_SEEN, false).apply();
+                editSharedPreference(SettingValues.PREF_SCROLL_SEEN, false);
             }
         });
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         storeNsfwHistorySwitch.setChecked(SettingValues.storeNSFWHistory);
         storeNsfwHistorySwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             SettingValues.storeNSFWHistory = isChecked;
-            SettingValues.prefs.edit().putBoolean(SettingValues.PREF_STORE_NSFW_HISTORY, isChecked).apply();
+            editSharedPreference(SettingValues.PREF_STORE_NSFW_HISTORY, isChecked);
         });
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         scrollSeenSwitch.setChecked(SettingValues.scrollSeen);
         scrollSeenSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             SettingValues.scrollSeen = isChecked;
-            SettingValues.prefs.edit().putBoolean(SettingValues.PREF_SCROLL_SEEN, isChecked).apply();
-
+            editSharedPreference(SettingValues.PREF_SCROLL_SEEN, isChecked);
         });
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //* Clear history */
         clearPostsLayout.setOnClickListener(v -> {
             KVStore.getInstance().clearTable();
-            new AlertDialogWrapper.Builder(context).setTitle(R.string.alert_history_cleared)
-                    .setPositiveButton(R.string.btn_ok, null).show();
+            showHistoryClearedAlertDialog();
         });
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         clearSubsLayout.setOnClickListener(v -> {
             UserSubscriptions.subscriptions.edit().remove("subhistory").apply();
-            new AlertDialogWrapper.Builder(context).setTitle(R.string.alert_history_cleared)
-                    .setPositiveButton(R.string.btn_ok, null).show();
+            showHistoryClearedAlertDialog();
         });
+    }
+
+    private void showHistoryClearedAlertDialog() {
+        new AlertDialog.Builder(context).setTitle(R.string.alert_history_cleared)
+                .setPositiveButton(R.string.btn_ok, null).show();
+    }
+
+    private void editSharedPreference(final String settingValueString, final boolean isChecked) {
+        SettingValues.prefs.edit().putBoolean(settingValueString, isChecked).apply();
     }
 }
